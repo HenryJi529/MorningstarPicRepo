ca9b10cd778b4001fd6ec12007f1a2ed66b2adb1
Henry Ji <jeep.jipu@gmail.com>
2025-07-07 16:51:57 +0800

    add all

--- 20250705/104620374-tasks.py
+++
@@ -1,7 +0,0 @@
-class Commands:
-    @staticmethod
-    def publicArchive():
-        colored_print("打包插件...")
-        runcmd(
-            "cd extension && make build && cd .. && zip -r release/extension_$(date '+%Y-%m-%d').zip extension/dist/*"
-        )
